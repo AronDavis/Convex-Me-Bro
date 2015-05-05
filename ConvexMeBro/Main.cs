3fcd0a304f1c822129f482d62bb7b31325724a99
Aron Davis <arontd@gmail.com>
2015-05-05 13:11:42 -0400

    Added checks and simplified code under the assumption that shape points are listed in clockwise order.

--- ConvexMeBro/Main.cs
+++ ConvexMeBro/Main.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace ConvexMeBro
 {
+    //ASSUMES THAT ALL POINTS ARE CLOCKWISE
     public partial class Main : Form
     {
         System.Drawing.Graphics g;
@@ -19,7 +20,7 @@ namespace ConvexMeBro
             InitializeComponent();
             g = this.CreateGraphics();
             pen = new Pen(Color.Red, 2);
-            txtInput.Text = "100,100/10,100/25,25/200,100/200,200/100,150/50,200";
+            txtInput.Text = "100,100/10,100/25,25/200,100/200,200/100,150/50,200/0,150/0,120";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,13 +32,15 @@ namespace ConvexMeBro
 
             if (shape == null) return;
 
-            if (isConvex(shape)) DrawLines(shape, Color.Blue);
+            if (isClockWiseConvex(shape)) DrawLines(shape, Color.Blue);
             else
             {
                 pen.Width = 4;
                 DrawLines(shape, Color.Red);
                 pen.Width = 2;
                 List<List<PointF>> convex = MakeConvex(shape);
+
+                if (hasDuplicates(convex)) MessageBox.Show("DUPLICATES FOUND");
 
                 foreach (List<PointF> points in convex)
                 {
@@ -46,129 +49,7 @@ namespace ConvexMeBro
             }
         }
 
-        private bool isConvex(List<PointF> shape)
-        {
-            bool set = false;
-            bool positive = false;
-            for(int i = 0; i < shape.Count; i++)
-            {
-                PointF p1 = shape[i];
-                PointF p2 = shape[(i + 1) % shape.Count];
-                PointF p3 = shape[(i + 2) % shape.Count];
-
-                PointF d1 = new PointF(p2.X - p1.X, p2.Y - p1.Y);
-                PointF d2 = new PointF(p3.X - p2.X, p3.Y - p2.Y);
-                
-                float cross = Helper.CrossProduct(d1, d2);
-
-                if (!set)
-                {
-                    if (cross != 0) set = true;
-                    positive = (cross > 0);
-                }
-                else if (positive != (cross > 0) && cross != 0) return false;
-            }
-
-            return true;
-        }
-
-        private List<List<PointF>> MakeConvex(List<PointF> shape)
-        {
-            List<List<PointF>> output = new List<List<PointF>>();
-
-            List<PointF> main = new List<PointF>(shape);
-
-            int last = 0;
-            bool set = false;
-            bool positive = false;
-            bool needLast = false;
-
-            for (int i = 0; i < shape.Count; i++)
-            {
-                PointF p1 = shape[i];
-                PointF p2 = shape[(i + 1) % shape.Count];
-                PointF p3 = shape[(i + 2) % shape.Count];
-
-                PointF d1 = new PointF(p2.X - p1.X, p2.Y - p1.Y);
-                PointF d2 = new PointF(p3.X - p2.X, p3.Y - p2.Y);
-
-                float cross = Helper.CrossProduct(d1, d2);
-
-                if (!set)
-                {
-                    if (cross != 0) set = true;
-                    positive = (cross > 0);
-                }
-                else if (positive != (cross > 0) && cross != 0) //if it turns the other way
-                {
-                    if(i - last > 3)
-                    {
-                        List<PointF> points = new List<PointF>(shape.GetRange(last, i - last));
-
-                        //ASSUMES THAT ALL POINTS ARE CLOCKWISE
-                        if (allPositive(points))
-                        {
-                            output.Add(points);
-
-                            for (int j = 1; j < points.Count - 1; j++)
-                            {
-                                if (main.Contains(points[j])) main.Remove(points[j]);
-                            }
-                        }
-                        
-                    }
-
-                    output.Add(new List<PointF>() { shape[(i - 1 + shape.Count) % shape.Count], p1, p2 });
-                    if (main.Contains(p1)) main.Remove(p1);
-                    last = i;
-                }
-            }
-
-            if (main.Count >= 3)
-            {
-                output.Add(main);
-            }
-
-            for (int i = output.Count - 1; i >= 0; i--)
-            {
-                if (!isConvex(output[i]))
-                {
-                    output.AddRange(MakeConvex(output[i]));
-                    output.RemoveAt(i);
-                }
-            }
-
-            return output;
-        }
-
-        private bool allPositive(List<PointF> shape)
-        {
-            bool set = false;
-            bool positive = false;
-            for (int i = 0; i < shape.Count; i++)
-            {
-                PointF p1 = shape[i];
-                PointF p2 = shape[(i + 1) % shape.Count];
-                PointF p3 = shape[(i + 2) % shape.Count];
-
-                PointF d1 = new PointF(p2.X - p1.X, p2.Y - p1.Y);
-                PointF d2 = new PointF(p3.X - p2.X, p3.Y - p2.Y);
-
-                float cross = Helper.CrossProduct(d1, d2);
-
-                if (!set)
-                {
-                    if (cross != 0) set = true;
-                    
-                    positive = (cross > 0);
-                }
-                else if (positive != (cross > 0) && cross != 0) return false;
-            }
-
-            return positive;
-        }
-
-
+        //convert text input into a list of PointF
         private List<PointF> convert(string[] text)
         {
             List<PointF> points = new List<PointF>();
@@ -187,6 +68,102 @@ namespace ConvexMeBro
             return points;
         }
 
+
+        //assumes points are listing in clockwise order
+        private bool isClockWiseConvex(List<PointF> shape)
+        {
+            for(int i = 0; i < shape.Count; i++)
+            {
+                PointF p1 = shape[i];
+                PointF p2 = shape[(i + 1) % shape.Count];
+                PointF p3 = shape[(i + 2) % shape.Count];
+
+                PointF d1 = new PointF(p2.X - p1.X, p2.Y - p1.Y);
+                PointF d2 = new PointF(p3.X - p2.X, p3.Y - p2.Y);
+                
+                float cross = Helper.CrossProduct(d1, d2);
+
+                //if we turn counter-clockwise
+                if (cross < 0) return false;
+            }
+
+            return true;
+        }
+
+        private List<List<PointF>> MakeConvex(List<PointF> shape)
+        {
+            List<List<PointF>> output = new List<List<PointF>>();
+
+            List<PointF> main = new List<PointF>(shape);
+
+            int last = 0;
+
+            for (int i = 0; i < shape.Count; i++)
+            {
+                PointF p1 = shape[i];
+                PointF p2 = shape[(i + 1) % shape.Count];
+                PointF p3 = shape[(i + 2) % shape.Count];
+
+                PointF d1 = new PointF(p2.X - p1.X, p2.Y - p1.Y);
+                PointF d2 = new PointF(p3.X - p2.X, p3.Y - p2.Y);
+
+                float cross = Helper.CrossProduct(d1, d2);
+
+                if (cross < 0) //if it turns counter-clockwise
+                {
+                    if(i - last > 3)
+                    {
+                        List<PointF> points = new List<PointF>(shape.GetRange(last, i - last));
+
+                        //Only consider points that are within our shape (CC shapes will be outside).
+                        if (isClockWiseConvex(points))
+                        {
+                            //add the points we missed (they should all have clockwise turns)
+                            output.Add(points);
+
+                            //remove the excess from main (we don't include start and end points because they're needed)
+                            for (int j = 1; j < points.Count - 1; j++)
+                            {
+                                if (main.Contains(points[j])) main.Remove(points[j]);
+                            }
+                        }
+                        
+                    }
+
+                    //add the "ear"
+                    output.Add(new List<PointF>() { shape[(i - 1 + shape.Count) % shape.Count], p1, p2 });
+
+                    //remove the trouble point from main
+                    if (main.Contains(p1)) main.Remove(p1);
+
+                    //mark last so we can later add the points that weren't troublesome
+                    last = i;
+                }
+            }
+
+            //only add main if it is a shape
+            if (main.Count >= 3)
+            {
+                output.Add(main);
+            }
+
+            //make sure all of the shapes we generated were convex
+            for (int i = output.Count - 1; i >= 0; i--)
+            {
+                //if the shape is not convex
+                if (!isClockWiseConvex(output[i]))
+                {
+                    //break it apart into convex shapes and add those shapes to the list
+                    output.AddRange(MakeConvex(output[i]));
+
+                    //remove the concave shape
+                    output.RemoveAt(i);
+                }
+            }
+
+            return output;
+        }
+
         private void DrawLines(List<PointF> points, Color color)
         {
             pen.Color = color;
@@ -196,6 +173,48 @@ namespace ConvexMeBro
                 PointF p2 = points[(i + 1) % points.Count];
                 g.DrawLine(pen, p1, p2);
             }
+        }
+
+        //check if there are any lists with the same content
+        private bool hasDuplicates(List<List<PointF>> mainList)
+        {
+            for(int i = 0; i < mainList.Count; i++)
+            {
+                for (int j = i+1; j < mainList.Count; j++)
+                {
+                    if (areDuplicates(mainList[i], mainList[j])) return true;
+                }
+            }
+
+            return false;
+        }
+
+        //assumes no duplicate points in a single list
+        private bool areDuplicates(List<PointF> list1, List<PointF> list2)
+        {
+            if (list1.Count != list2.Count) return false;
+
+            //go through list1
+            for(int i = 0; i < list1.Count; i++)
+            {
+                bool foundMatch = false;
+
+                //go through list2
+                for (int j = 0; j < list2.Count; j++)
+                {
+                    //if the X's and Y's are the same
+                    if(list1[i].X == list2[j].X && list1[i].Y == list2[j].Y)
+                    {
+                        foundMatch = true;
+                        break;
+                    }
+                }
+
+                //didn't find a match, can't be duplicates
+                if (!foundMatch) return false;
+            }
+
+            return true;
         }
     }
 }
